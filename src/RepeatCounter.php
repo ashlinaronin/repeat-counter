1b86818b678019b8a5f2bdd908d8124e297c1044
Ashlin Aronin <ashlinaronin@gmail.com>
2015-08-14 10:12:03 -0700

    Make mixed-case phrase spec pass

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,9 +4,9 @@ class RepeatCounter
 {
     function countRepeats($word_to_find, $phrase)
     {
-        $phrase_words = explode(" ", $phrase);
-        $occurences = 0;
+        $phrase_words = explode(" ", strtolower($phrase));
         $word_to_find = strtolower($word_to_find);
+        $occurences = 0;
 
         foreach ($phrase_words as $word) {
             if ($word == $word_to_find) {
