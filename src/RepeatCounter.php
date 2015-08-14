bbdb876f037f45e952c025070d6419120b56ae89
Ashlin Aronin <ashlinaronin@gmail.com>
2015-08-14 10:19:08 -0700

    Make ignore punctuation in phrase spec pass

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,6 +4,10 @@ class RepeatCounter
 {
     function countRepeats($word_to_find, $phrase)
     {
+        // Strip all punctuation from phrase using find and replace regular expression
+        // Arg 1 is pattern, arg 2 is replacement, arg 3 is subject to act on
+        $phrase = preg_replace("#[[:punct:]]#", "", $phrase);
+
         $phrase_words = explode(" ", strtolower($phrase));
         $word_to_find = strtolower($word_to_find);
         $occurences = 0;
