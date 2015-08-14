e684db809fbeb17685d61f0156d870550c95b109
Ashlin Aronin <ashlinaronin@gmail.com>
2015-08-14 09:58:22 -0700

    Add once beginning spec and make it fail

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -4,7 +4,7 @@
 
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
-        function test_countRepeatsEquivalent()
+        function test_countRepeats_equivalent()
         {
             //Arrange
             $test_RepeatCounter = new RepeatCounter;
@@ -17,6 +17,24 @@
             //Assert
             $this->assertEquals(1, $result);
         }
+
+        function test_countRepeats_onceBeginning()
+        {
+            //Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $input_word = "the";
+            $input_phrase = "the quick brown fox";
+
+            //Act
+            $result = $test_RepeatCounter->countRepeats($input_word, $input_phrase);
+
+            //Assert
+            $this->assertEquals(1, $result);
+        }
+
+
+
+
     }
 
 
