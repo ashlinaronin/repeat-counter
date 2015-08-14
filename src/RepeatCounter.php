f7e5b5fd7ed88d4ef793b486e2b31e9db1aa19ed
Ashlin Aronin <ashlinaronin@gmail.com>
2015-08-14 10:33:45 -0700

    Make special chars word spec pass

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,21 +4,29 @@ class RepeatCounter
 {
     function countRepeats($word_to_find, $phrase)
     {
-        // Strip all punctuation from phrase using find and replace regular expression
-        // Arg 1 is pattern, arg 2 is replacement, arg 3 is subject to act on
-        $phrase = preg_replace("#[[:punct:]]#", "", $phrase);
+        // Make sure word_to_find is alphanumeric before executing anything else
+        if (ctype_alnum($word_to_find)) {
 
-        $phrase_words = explode(" ", strtolower($phrase));
-        $word_to_find = strtolower($word_to_find);
-        $occurences = 0;
+            // Strip all non-alphanumeric chars from phrase using find and replace regular expression
+            // Arg 1 is pattern, arg 2 is replacement, arg 3 is subject to act on
+            $phrase = preg_replace("/[^\da-z ]/i", "", $phrase);
 
-        foreach ($phrase_words as $word) {
-            if ($word == $word_to_find) {
-                $occurences++;
+            $phrase_words = explode(" ", strtolower($phrase));
+            $word_to_find = strtolower($word_to_find);
+            $occurences = 0;
+
+            foreach ($phrase_words as $word) {
+                if ($word == $word_to_find) {
+                    $occurences++;
+                }
             }
-        }
 
-        return $occurences;
+            return $occurences;
+
+        } else {
+            // If $word_to_find has non-alphanumeric characters, return an error
+            return -1;
+        }
     }
 }
 
